0ba8aee594fe77845a8667615110b5c5413cff9f
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-03-01 09:21:25 +0100

    URL testing
    seeing if I can load an url from an unity application, it works.

---
+++ IWP_UnityProject/Assets/testing.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testing : MonoBehaviour
+{
+    public void ButtonClick(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
