ec85d342e171ace0a98535756bdce4601e823f62
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-03-16 10:34:35 +0100

    Tried out text recognition
    String.Contains("desiredString");

---
+++ IWP_UnityProject/Assets/Scripts/Testing_Chatbot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Testing_Chatbot : MonoBehaviour
+{
+    [SerializeField] private Text PlayerText;
+
+    public void Typing()
+    {
+        Debug.Log("Still typing...");
+
+    }
+
+    public void Done()
+    {
+        Debug.Log("Done typing");
+        CheckText();
+    }
+
+    private void CheckText()
+    {
+        if (PlayerText.text.Contains("test") || PlayerText.text.Contains("Test"))
+        {
+            Debug.Log("contains test or Test");
+        }
+    }
+}
--- IWP_UnityProject/Assets/Scripts/Testing_WebLoading.cs
+++ IWP_UnityProject/Assets/Scripts/Testing_WebLoading.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class testing : MonoBehaviour
+public class Testing_WebLoading : MonoBehaviour
 {
     public void ButtonClick(string url)
     {
