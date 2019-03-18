41a06cb5378f32dbeec8bc8a36646363cc39aa78
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-03-18 11:18:07 +0100

    Finish Rough Idea

--- IWP_UnityProject/Assets/Scripts/Testing_Chatbot.cs
+++ IWP_UnityProject/Assets/Scripts/Testing_Chatbot.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class Testing_Chatbot : MonoBehaviour
 {
-    [SerializeField] private Text PlayerText;
+    [SerializeField] private Text playerInputText;
+
+    [SerializeField] private Transform textSpawnTransform;
 
     public void Typing()
     {
@@ -16,14 +18,48 @@ public class Testing_Chatbot : MonoBehaviour
     public void Done()
     {
         Debug.Log("Done typing");
+        SpawnText(playerInputText.text, true);
         CheckText();
     }
 
     private void CheckText()
     {
-        if (PlayerText.text.Contains("test") || PlayerText.text.Contains("Test"))
+        if (playerInputText.text.Contains("404"))
         {
-            Debug.Log("contains test or Test");
+            SpawnText("Hallo, ik had een afspraak. Mijn naam is <GenericName>.");
+            SpawnText("Ik heb een probleem met het maken van deze game.");
         }
+        else if (playerInputText.text.Contains("probleem"))
+        {
+            SpawnText("Het probleem is dat de code een bende wordt en ik nog geen idee heb hoe ik het schoon kan houden.");
+        }
+        else if (playerInputText.text.Contains("vaktaal"))
+        {
+            SpawnText("Ik kan ook nog niet goed bezig omdat ik nog geen voorbeeld heb van een casus.");
+        }
+        else
+        {
+            SpawnText("Sorry, ik begrijp niet goed wat u zegt, kunt u het misschien anders verwoorden?");
+        }
+    }
+
+    private void SpawnText(string _output, bool _player = false)
+    {
+        GameObject _newText = new GameObject(_output.Replace(" ", "-"), typeof(RectTransform));
+        var _newTextComp = _newText.AddComponent<Text>();
+
+        _newTextComp.text = _output;
+        _newTextComp.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        _newTextComp.color = Color.black;
+        _newTextComp.horizontalOverflow = HorizontalWrapMode.Wrap;
+        _newTextComp.verticalOverflow = VerticalWrapMode.Overflow;
+
+        if (_player)
+        {
+            _newTextComp.alignment = TextAnchor.UpperRight;
+        }
+
+        _newText.GetComponent<RectTransform>().sizeDelta = new Vector2(260, 32);      
+        _newText.transform.SetParent(textSpawnTransform);
     }
 }
