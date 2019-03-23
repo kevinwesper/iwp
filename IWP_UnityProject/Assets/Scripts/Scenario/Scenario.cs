c64a31e0914503e16e7e0bd4a4f2cbd703fce232
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-03-23 12:45:53 +0100

    Variable test

---
+++ IWP_UnityProject/Assets/Scripts/Scenario/Scenario.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; //[Serializable]
+
+[CreateAssetMenu]
+public class Scenario : ScriptableObject
+{
+    [Header("Answer settings")]
+    [Tooltip("Is the client missing information?")]
+    public bool isMissing;
+    [Tooltip("Is the client's case hopeless?")]
+    public bool isHopeless;
+    [Tooltip("Is the client lying?")]
+    public bool isLiar;
+
+    // public bool isScared;
+    // public bool isSad;
+    // public bool isConfused;
+
+    [Tooltip("List of good arguments for this case.")]
+    public string[] goodArguments;
+
+    [Header("Story settings")]
+    [Tooltip("Name of the client.")]
+    public string clientName;
+    [Tooltip("Introductionary problem statement of the client.")]
+    [TextArea] public string clientintro;
+}
