1aa822053b0830c5c1b1cb4eba6525b8cefee60b
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-03-25 11:45:12 +0100

    Made a score variable type

--- IWP_UnityProject/Assets/Scripts/Scenario/Scenario.cs
+++ IWP_UnityProject/Assets/Scripts/Scenario/Scenario.cs
@@ -6,8 +6,17 @@ using System;
 [Serializable]
 public class Reaction
 {
-    public string keyword;
-    [TextArea] public string reaction;
+    [Tooltip("Keyword for the reaction")] public string keyword;
+
+    [Tooltip("Reaction to the keyword")] [TextArea] public string reaction;
+}
+
+[Serializable]
+public class Arguments
+{
+    [Tooltip("Argument keyword")] public string argument;
+
+    [Tooltip("Argument score")] public int score;
 }
 
 [CreateAssetMenu]
@@ -16,10 +25,8 @@ public class Scenario : ScriptableObject
     #region Scenario Settings
     [Header("Scenario Settings")]
 
-    [Tooltip("Is the client missing information?")]
-    public bool isMissing;
-    [Tooltip("Is the client's case hopeless?")]
-    public bool isHopeless;
+    [Tooltip("Is the client missing information?")] public bool isMissing;
+    [Tooltip("Is the client's case hopeless?")] public bool isHopeless;
 
     //public bool isLiar;
     // public bool isScared;
@@ -30,8 +37,9 @@ public class Scenario : ScriptableObject
     #region Answer Settings
     [Header("Answer Settings")]
     
-    [Tooltip("List of good arguments for this case.")]
-    public string[] goodArguments;
+    [Tooltip("List of good arguments for this case.")] public string[] goodArguments; // Give points per answer, amount of points needed to win???
+
+    [Tooltip("List of good argmument keywords with score.")] public Arguments[] arguments;
     #endregion
 
     #region Story Settings
